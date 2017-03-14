df5955c999622bef6593f34c7910e97bcfb2fd4f
ErwanGF <erwangf@gmail.com>
2017-03-14 14:31:50 +0100

    Meilleur affichage

--- src/web/js/graphViz.js
+++ src/web/js/graphViz.js
@@ -28,22 +28,23 @@ let articles = [];
 let idPosition = 1;
 let data = [];
 
+let lolzActive = true;
 
 
-function loadData(f,callback){
+function loadData(f, callback) {
 
 
     let myConfig = config;
 
-    myConfig.complete =  function(results, file) {
+    myConfig.complete = function (results, file) {
         console.log("Parsing complete:", results, file);
 
 
-        results.data.forEach((r)=>{
+        results.data.forEach((r)=> {
             //find all articles
             let articleID = r[articleIDField];
-            let articleAlreadyExists = articles.filter(a=> a==articleID).length!=0;
-            if(!articleAlreadyExists) articles.push(articleID);
+            let articleAlreadyExists = articles.filter(a=> a == articleID).length != 0;
+            if (!articleAlreadyExists) articles.push(articleID);
             data.push(r);
         });
 
@@ -56,23 +57,27 @@ function loadData(f,callback){
 }
 
 
+function createNetwork(articleFilter) {
 
-function createNetwork(articleFilter){
-    let nodesArray = [{id:1,label:"null"}];
+    let nodesArray = [{id: 1, label: "null"}];
     let edgesArray = [];
 
-    data.filter(v=>v[articleIDField]==articleFilter).forEach(function(r){
+    data.filter(v=>v[articleIDField] == articleFilter).forEach(function (r) {
         //check if r.user is in the nodesArray
-        if(nodesArray.filter(r1=>r1.label==r[userField]).length==0){
+        if (nodesArray.filter(r1=>r1.label == r[userField]).length == 0) {
             idPosition++;
-            nodesArray.push({id:idPosition,label:r[userField]});
+            nodesArray.push({id: idPosition, label: r[userField]});
         }
 
     });
-    data.filter(v=>v[articleIDField]==articleFilter).forEach(function(r){
-        let idFrom = nodesArray.filter(function(r1){return r1.label==r[parentIdField]})[0].id;
-        let idTo = nodesArray.filter(function(r1){return r1.label==r[userField]})[0].id;
-        edgesArray.push({from:idFrom,to:idTo});
+    data.filter(v=>v[articleIDField] == articleFilter).forEach(function (r) {
+        let idFrom = nodesArray.filter(function (r1) {
+            return r1.label == r[parentIdField]
+        })[0].id;
+        let idTo = nodesArray.filter(function (r1) {
+            return r1.label == r[userField]
+        })[0].id;
+        edgesArray.push({from: idFrom, to: idTo});
     });
 
     let nodes = new vis.DataSet(nodesArray);
@@ -88,10 +93,10 @@ function createNetwork(articleFilter){
             hierarchical: {
                 direction: "UD",
                 sortMethod: "directed",
-                nodeSpacing : 30
+                nodeSpacing: 30
             }
         },
-        interaction: {dragNodes :true},
+        interaction: {dragNodes: true},
         physics: {
             enabled: false
         },
@@ -100,32 +105,42 @@ function createNetwork(articleFilter){
                 return path.indexOf('hierarchical') !== -1;
 
             },
-            showButton:false
+            showButton: false
         }
     };
     let network = new vis.Network(container, d, options);
+
+    container.className = lolzActive ? "lolz" : "";
+
 }
 
-let onFileChanged = function(files){
-    loadData(files[0],actualizeSelect);
+let onFileChanged = function (files) {
+    loadData(files[0], actualizeSelect);
 
 };
 
-const actualizeSelect = function(){
+const actualizeSelect = function () {
     let option = '';
-    for (let i=0;i<articles.length;i++){
-        option += '<option value="'+ articles[i] + '">' + articles[i] + '</option>';
+    for (let i = 0; i < articles.length; i++) {
+        option += '<option value="' + articles[i] + '">' + articles[i] + '</option>';
     }
     $('#filterSelect').append(option);
 }
 
-let loadButtonAction = function(){
+let loadButtonAction = function () {
 
     let filter = $("#filterSelect")[0].value;
 
-    if(filter.length !=0 && data.length!=0){
-       createNetwork(filter);
+
+    if (filter.length != 0 && data.length != 0) {
+        createNetwork(filter);
     }
 
 }
+
+let lolzOnCHanged = function (value) {
+    console.log(value)
+    let container = document.getElementById('network');
+    lolzActive = value;
+};
 
